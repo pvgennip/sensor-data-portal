2d8b7804128965eaa80a980c3a910599025839e6
Pim van Gennip <pim@iconize.nl>
2017-03-01 12:15:16 +0100

    Added run actions for initialising portal Laravel app

--- run_actions.sh
+++ run_actions.sh
@@ -7,10 +7,11 @@ cd $base_dir/laradock
 docker-compose stop
 docker-compose up -d --build apache2 php-fpm mysql phpmyadmin influx workspace
 
-# laravel
-cd $base_dir/portal
-composer install && sudo chmod -R 777 storage && sudo chmod -R 777 bootstrap/cache && php artisan migrate
+# laravel set up and build
+cd $base_dir
+docker exec -it laradock_workspace_1 script /dev/null -c "if [ ! -f '.env' ]; then cp .env.example .env && php artisan key:generate; fi"
+docker exec -it laradock_workspace_1 script /dev/null -c "composer install && chmod -R 777 storage && chmod -R 777 bootstrap/cache && php artisan migrate"
 
-# bower
+# angular app 
 cd $base_dir/portal
 cd public/app && bower install --allow-root
