c866337636256c50af5096c05a0ca91fec02fe12
Pim van Gennip <pim@iconize.nl>
2017-03-17 17:07:13 +0100

    Tweaks for bower installation

--- run_actions_no_cache.sh
+++ run_actions_no_cache.sh
@@ -5,8 +5,8 @@ base_dir=$(pwd)
 # docker
 cd $base_dir/laradock
 docker-compose stop
-docker-compose build --no-cache apache2 php-fpm mysql phpmyadmin influx workspace
-docker-compose up -d apache2 php-fpm mysql phpmyadmin influx workspace
+docker-compose build --no-cache apache2 php-fpm mysql phpmyadmin influx bower workspace
+docker-compose up -d apache2 php-fpm mysql phpmyadmin influx bower workspace
 
 # laravel set up and build
 cd $base_dir
