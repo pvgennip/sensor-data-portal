55a2c9bd74554a2ce3f7657988b849bc8cc87103
Pim van Gennip <pim@iconize.nl>
2017-03-20 11:49:22 +0100

    Updated no cache actions

--- run_actions_no_cache.sh
+++ run_actions_no_cache.sh
@@ -5,13 +5,16 @@ base_dir=$(pwd)
 # docker
 cd $base_dir/laradock
 docker-compose stop
-docker-compose build --no-cache apache2 php-fpm mysql phpmyadmin influx bower workspace
-docker-compose up -d apache2 php-fpm mysql phpmyadmin influx bower workspace
+docker-compose build --no-cache apache2 php-fpm mysql phpmyadmin influx workspace
+docker-compose up -d apache2 php-fpm mysql phpmyadmin influx workspace
 
 # laravel set up and build
 cd $base_dir
 docker exec -it laradock_workspace_1 script /dev/null -c "if [ ! -f '.env' ]; then cp .env.example .env && php artisan key:generate; fi"
 docker exec -it laradock_workspace_1 script /dev/null -c "composer install && chmod -R 777 storage && chmod -R 777 bootstrap/cache && php artisan migrate"
 
-# angular app 
-docker exec -it laradock_workspace_1 script /dev/null -c "cd portal/public/webapp && npm install -g bower && bower install"
+# angular app (switch to laradock user with 'su laradock' to have access to node, npm, bower)
+cd $base_dir
+docker exec -it laradock_workspace_1 bash 
+cd public/webapp && mkdir vendor && bower install --allow-root
+exit
