72bf314e577e642a08dabe8e14a6d0bafaed4456
Pim van Gennip <pim@iconize.nl>
2017-03-20 11:45:56 +0100

    Added bower install by interactive bash session

--- run_actions.sh
+++ run_actions.sh
@@ -12,5 +12,9 @@ cd $base_dir
 docker exec -it laradock_workspace_1 script /dev/null -c "if [ ! -f '.env' ]; then cp .env.example .env && php artisan key:generate; fi"
 docker exec -it laradock_workspace_1 script /dev/null -c "composer install && chmod -R 777 storage && chmod -R 777 bootstrap/cache && php artisan migrate"
 
-# angular app 
-docker exec -it laradock_workspace_1 script /dev/null -c "cd public/webapp && bower install"
+# angular app (switch to laradock user with 'su laradock' to have access to node, npm, bower)
+cd $base_dir
+docker exec -it laradock_workspace_1 bash 
+cd public/webapp && mkdir vendor && bower install --allow-root
+exit
+
