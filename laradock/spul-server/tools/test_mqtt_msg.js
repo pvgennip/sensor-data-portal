ee89d2c52953e51df5b8c53b96bf6734b88d1394
Pim van Gennip <pim@iconize.nl>
2017-04-28 16:37:49 +0200

    Added MQTTWarn and Mosquito config

--- laradock/spul-server/tools/test_mqtt_msg.js
+++ laradock/spul-server/tools/test_mqtt_msg.js
@@ -1,5 +1,5 @@
 const mqtt = require('mqtt');
-const mqttHost = (process.env['MQTT_HOST'] || 'localhost')
+const mqttHost = (process.env['MQTT_HOST'] || '37.48.120.157')
 var client = mqtt.connect('mqtt://' + mqttHost, {clientId: "0000000000000998", password: "", connectTimeout: 3000, })
-client.on('connect', () => { client.publish('data', 'Final is een test') })
+client.on('connect', () => { client.publish('data', 'Hey, weer is een test') })
 client.end()
