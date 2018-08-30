cd83f4dc3167c64273b67c840103954db34b40ac
Pim van Gennip <pim@iconize.nl>
2018-08-31 01:10:24 +0200

    Adjusted run_actions

--- backup.sh
+++ backup.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 
-cd /root/sensor-data-portal
+mkdir /root/backups
 
-mkdir backups
+# Tell mysql container to make backup 
+mysqldump -u akvo -p'o3qxQfejCKWYBd4y' sensors-akvo-org | gzip > /root/backups/sensors-akvo-org.sql.gz
 
-mkdir /var/lib/docker/volumes/laradock_mysql/_data/backups
-# Tell mysql container to make backup (Without -it for calling from cron!!)
-docker exec laradock_mysql_1 bash -c "mysqldump -u homestead -p'secret' homestead | gzip > /var/lib/mysql/backups/homestead.sql.gz"
-mv /var/lib/docker/volumes/laradock_mysql/_data/backups/*.gz backups
-
-
-mkdir /var/lib/docker/volumes/laradock_influx/_data/backups
-# Tell Influx container to make backup (Without -it for calling from cron!!)
-docker exec laradock_influx_1 bash -c "influxd backup -database sensordata /var/lib/influxdb/backups"
-tar -zcvf /var/lib/docker/volumes/laradock_influx/_data/influx_sensordata.tar.gz /var/lib/docker/volumes/laradock_influx/_data/backups
-mv /var/lib/docker/volumes/laradock_influx/_data/*.gz backups
-rm /var/lib/docker/volumes/laradock_influx/_data/backups/*
+# Tell Influx container to make backup 
+influxd backup -database sensordata /tmp/influxdb/backups
+tar -zcvf /root/backups/influx_sensordata.tar.gz /tmp/influxdb/backups
+rm /tmp/influxdb/backups
 
 exit 0
