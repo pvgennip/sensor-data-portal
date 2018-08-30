c199c1134d75e27ecc36b540fa4aff1dd5d4d141
Pim van Gennip <pim@iconize.nl>
2018-08-31 01:22:39 +0200

    updated backup script

--- backup.sh
+++ backup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-mkdir /root/backups
+sudo mkdir /root/backups
 
 # Tell mysql container to make backup 
 mysqldump -u akvo -p'o3qxQfejCKWYBd4y' sensors-akvo-org | gzip > /root/backups/sensors-akvo-org.sql.gz
 
 # Tell Influx container to make backup 
+sudo mkdir /tmp/influxdb/backups
 influxd backup -database sensordata /tmp/influxdb/backups
 tar -zcvf /root/backups/influx_sensordata.tar.gz /tmp/influxdb/backups
-rm /tmp/influxdb/backups
+sudo rm /tmp/influxdb/backups
 
 exit 0
